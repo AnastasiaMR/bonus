10fee57af84d2249214caa8da6ae7f24c1254069
Anastasia Matryashina <matr-nastya@yandex.ru>
2021-02-18 23:19:02 +0300

    initial commit

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        long balance = 100;
+        long refill = 1336;
+        long bonus = 0;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        }
+
+        long new_balance = balance + bonus + refill;
+
+        System.out.println("На вашем счету " + new_balance + " рублей");
+    }
+}
